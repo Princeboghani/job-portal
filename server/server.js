1ed0142b272df5a8d14357ae2a3520ea273793c1
Prince Boghani <boghaniprinsh@gmail.com>
2025-05-29 11:44:28 +0530

    hiiii

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-app.get("/", (req, res) => res.send("api is working not working"));
+app.get("/", (req, res) => res.send("api is working not working thats"));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
